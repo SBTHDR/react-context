1024c9e08d2bf75ce314f5c21442c8ed4ed0c6b1
Subrata <subrata.halder13d@gmail.com>
2022-06-09 12:00:37 +0600

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { GlobalProvider } from './context/GlobalState';
+import { Home } from './components/Home';
+import { AddEmployee } from './components/AddEmployee';
+import { EditEmployee } from './components/EditEmployee';
+
+const App = () => {
+  return (
+    <GlobalProvider>
+      <div className="App">
+        <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/add" component={AddEmployee} exact />
+            <Route path="/edit/:id" component={EditEmployee} exact />
+        </Switch>
+      </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
